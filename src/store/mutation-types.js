b48f5058a5227ae88d98c7d7230a22b1bff57484
asdas012345 <1170971051@qq.com>
2020-04-24 16:01:11 +0800

    vue.js音乐播放器

---
+++ src/store/mutation-types.js
@@ -0,0 +1,31 @@
+/*
+ * @Author: your name
+ * @Date: 2020-03-05 14:42:02
+ * @LastEditTime: 2020-03-08 16:15:57
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \vscodee:\迅雷下载\vue-music-master\vue-music-master\src\store\mutation-types.js
+ */
+export const SET_SINGER = 'SET_SINGER'
+
+export const SET_PLAYING_STATE = 'SET_PLAYING_STATE'
+
+export const SET_FULL_SCREEN = 'SET_FULL_SCREEN'
+
+export const SET_PLAYLIST = 'SET_PLAYLIST'
+
+export const SET_SEQUENCE_LIST = 'SET_SEQUENCE_LIST'
+
+export const SET_PLAY_MODE = 'SET_PLAY_MODE'
+
+export const SET_CURRENT_INDEX = 'SET_CURRENT_INDEX'
+
+export const SET_DISC = 'SET_DISC'
+
+export const SET_TOP_LIST = 'SET_TOP_LIST'
+
+export const SET_SEARCH_HISTORY = 'SET_SEARCH_HISTORY'
+
+export const SET_PLAY_HISTORY = 'SET_PLAY_HISTORY'
+
+export const SET_FAVORITE_LIST = 'SET_FAVORITE_LIST'
